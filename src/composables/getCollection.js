858c968a65a6e67784af26e6f09d71944dd4a62e
Elvan Hidayat <elvanhd@gmail.com>
2024-02-14 12:01:48 +0700

    Enhance chat interface and improve code robustness
    - Auto-scroll to bottom in ChatWindow when new messages arrive
    - Implement unsubscription from Firestore updates on component unmount
    - Add navigation guard to Welcome route for authenticated users
    - Utilize Vue's ref and onUpdated to manage DOM interactions
    - WatchEffect used for cleanup and efficient resource management
    - Ensure users are redirected to Chatroom if already logged in
    - Optimize chat message fetching with real-time updates and cleanup

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { projectFirestore } from '../firebase/config';
 
 const getCollection = (collection) => {
@@ -8,8 +8,9 @@ const getCollection = (collection) => {
   // register the firestore collection reference
   let collectionRef = projectFirestore.collection(collection).orderBy('createdAt');
 
-  collectionRef.onSnapshot(
+  const unsub = collectionRef.onSnapshot(
     (snap) => {
+      console.log(snap);
       let results = [];
       snap.docs.forEach((doc) => {
         // must wait for the server to create the timestamp & send it back
@@ -27,6 +28,11 @@ const getCollection = (collection) => {
       error.value = 'could not fetch the data';
     }
   );
+
+  watchEffect((onInvalidate) => {
+    // unsub from prev collection when watcher is stopped (component unmounted)
+    onInvalidate(() => unsub());
+  });
 
   return { error, documents };
 };
