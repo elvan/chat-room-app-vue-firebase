081da25d11078d19dde455fdf0c042ab43df354b
Elvan Hidayat <elvanhd@gmail.com>
2024-02-14 08:11:37 +0700

    Add project setup and initial configuration
    - Add new configuration files: .browserslistrc, .editorconfig, .gitignore
    - Introduce babel, jsconfig, and package.json for project setup
    - Implement initial Vue app structure with App.vue and main.js
    - Create router setup with index.js and Welcome view
    - Include global CSS styling in main.css
    - Add public assets: favicon.ico and index.html template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+#app {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome container">
+    <p>welcome</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.welcome {
+  padding: 20px 0;
+  text-align: center;
+}
+</style>
